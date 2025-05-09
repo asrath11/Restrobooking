170e3ed2dd2c30c2a4cdf6ec172572aeaf2c6455
Asrath <pasrath.2004@gmail.com>
2025-05-09 20:45:18 +0530

    updated  popup component

--- client/src/components/PopUpSection/PopUp.jsx
+++ client/src/components/PopUpSection/PopUp.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function PopUp({ icon: Icon, desc }) {
   return (
-    <div className='absolute top-40 left-1/2 -translate-x-1/2 w-[28%] p-4 flex gap-4 items-center bg-black text-white rounded-2xl shadow-lg px-40 animate-popup opacity-0'>
-      <Icon className='text-green-500 text-2xl mr-3' />
-      <span>{desc}</span>
+    <div className='absolute top-20 md:top-40 left-1/2 -translate-x-1/2 min-w-[300px] max-w-md w-full mx-4 md:mx-auto p-4 flex flex-col md:flex-row gap-4 items-center bg-black text-white rounded-2xl shadow-lg px-4 md:px-8 animate-popup opacity-0'>
+      <Icon className='text-green-500 text-2xl mb-2 md:mb-0 md:mr-3' />
+      <span className='text-sm md:text-base text-center md:text-left'>
+        {desc}
+      </span>
     </div>
   );
 }
