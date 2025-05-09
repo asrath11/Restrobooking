492235898597405d67cc199c5887bd2cdf87dc97
Asrath <pasrath.2004@gmail.com>
2025-05-09 20:57:30 +0530

    updated  popup component

--- client/src/components/PopUpSection/PopUp.jsx
+++ client/src/components/PopUpSection/PopUp.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function PopUp({ icon: Icon, desc }) {
   return (
-    <div className='absolute top-20 md:top-40 left-1/2 -translate-x-1/2 min-w-[300px] max-w-md w-full mx-4 md:mx-auto p-4 flex flex-col md:flex-row gap-4 items-center bg-black text-white rounded-2xl shadow-lg px-4 md:px-8 animate-popup opacity-0'>
-      <Icon className='text-green-500 text-2xl mb-2 md:mb-0 md:mr-3' />
-      <span className='text-sm md:text-base text-center md:text-left'>
+    <div className='absolute top-20 md:top-40 left-1/2 -translate-x-1/2 min-w-[300px] max-w-md w-full mx-4 md:mx-auto p-4 flex flex-col sm:flex-row gap-3 items-center bg-black text-white rounded-2xl shadow-lg px-4 md:px-8 animate-popup opacity-0'>
+      <Icon className='text-green-500 text-xl sm:text-2xl flex-shrink-0 mb-1 sm:mb-0 sm:mr-2' />
+      <span className='text-sm sm:text-base text-center sm:text-left'>
         {desc}
       </span>
     </div>
