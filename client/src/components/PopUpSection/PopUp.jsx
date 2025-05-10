eccc9382189c998d3d0ec775ed389b970ce1c2bc
Asrath <pasrath.2004@gmail.com>
2025-05-10 18:33:51 +0530

    updated the pooup component

--- client/src/components/PopUpSection/PopUp.jsx
+++ client/src/components/PopUpSection/PopUp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const PopUp = ({ icon: Icon, desc }) => (
   <div className='absolute top-35 left-1/2 -translate-x-1/2 xl:w-[15%] flex gap-5 items-center space-x-2 bg-green-300 p-3 rounded-lg animate-popup opacity-0'>
     <Icon className='text-green-700 w-12 h-10' />
-    <h1 className='font-medium'>{desc}</h1>
+    <h1 className='font-medium text-black'>{desc}</h1>
   </div>
 );
 
